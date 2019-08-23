f7e30291f6ecc3ba4cf86ed0cdc6d0de1f632b06
FridaPolished <fridapulido@gmail.com>
2019-08-23 13:53:28 -0700

    Phase 1 and 2 of B.Y.O.A Co-Author: Ryan <ryanwoods14@gmail.com>

--- Buiding Active RecordLite/lib/01_sql_object.rb
+++ Buiding Active RecordLite/lib/01_sql_object.rb
@@ -1,9 +1,13 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
+
+
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
+
   def self.columns
     if @columns
       @columns
@@ -43,6 +47,7 @@ class SQLObject
   end
 
   def self.all
+    
     results = DBConnection.execute(<<-SQL)
     SELECT
       *
@@ -54,6 +59,7 @@ class SQLObject
   end
 
   def self.parse_all(results)
+     
     all_objects = []
     results.each do |params|
       all_objects << self.new(params)
@@ -73,16 +79,17 @@ class SQLObject
     LIMIT
       1
     SQL
-     
+     #debugger 
     return nil if result.empty?
     self.parse_all(result).first 
   end
 
   def initialize(params = {})
     #raise "No parameters given"if params.empty?
-
+    
     params.each do |k,v|
-      if self.class.columns.include?(k.to_sym)
+      k = k.to_sym
+      if self.class.columns.include?(k)
          self.send("#{k}=", v)
       else
         raise "unknown attribute '#{k}'"
@@ -96,28 +103,55 @@ class SQLObject
 
   def attribute_values
     atr_vals = self.class.columns
-      atr_vals.map! do |val|
+      atr_vals.map do |val|
           self.send(val)
       end
   end
 
   def insert
-    col = self.class.columns
-    values = self.attribute_values
-    result = DBConnection.execute(<<-SQL, *values)
+   #debugger 
+    col = self.class.columns.drop(1).map {|col| col.to_s}
+    col_str = col.join(", ")
+    values = self.attribute_values.drop(1)
+    ques_str = "?, "* (values.length-1) + "?" 
+     
+     
+    
+    DBConnection.execute(<<-SQL, *values)
     INSERT INTO
-     #{self.table_name} ()
+     #{self.class.table_name} (#{col_str})
     VALUES
-      ?
+      (#{ques_str})
     SQL
-    result 
+   
+    self.id = DBConnection.last_insert_row_id
+    #  debugger
   end
 
   def update
-    # ...
+    col = self.class.columns.drop(1).map {|col| col.to_s} 
+    col_str = col.join(" = ?, ")
+    out_str = col_str + " = ?"
+    id_x = self.attribute_values.first
+    values = self.attribute_values.drop(1)
+    values << id_x   
+    
+    DBConnection.execute(<<-SQL, *values)
+    UPDATE
+     #{self.class.table_name} 
+    SET 
+    #{out_str}
+    WHERE
+      id = ?
+    SQL
+   
   end
 
   def save
-    # ...
+    if self.id.nil?
+      self.insert 
+    else
+      self.update
+    end
   end
 end
--- Buiding Active RecordLite/lib/02_searchable.rb
+++ Buiding Active RecordLite/lib/02_searchable.rb
@@ -1,12 +1,29 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
-
+require 'byebug'
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map {|k, v| "#{k} = ?" }
+    where_line_str = where_line.join(" AND ")
+    values = params.values
+
+    result = DBConnection.execute(<<-SQL, *values)
+    SELECT
+      id
+    FROM
+      #{self.table_name}
+    WHERE
+      #{where_line_str}
+    SQL
+    #debugger 
+    objects = []
+    result.each do |object_id|
+        objects << self.find(object_id["id"])
+    end
+    objects
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
